965288a0571b84902191962df872209f95fb52b3
Sushanth BC <sushanthbc@gmail.com>
2017-12-28 10:55:59 +0530

    issue5: top-navigation-fix

--- app/javascript/bundles/components/AccountForm/AccountForm.jsx
+++ app/javascript/bundles/components/AccountForm/AccountForm.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import $ from 'jquery';
+import TopNavigation from '../TopNavigation/TopNavigation';
 
 class AccountForm extends React.Component {
   render () {
     return (
-      <div className="right-pane-container">
-        <form className="form-vertical">
-          <div className="form-group col-md-5">
-            <label> Pharmacy Name </label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group col-md-5">
-            <label>Street</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group col-md-5">
-            <label> Area </label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group col-md-5">
-            <label> City </label>
-            <input type="text" className="form-control" />
-          </div>
-        </form>
+      <div>
+        <TopNavigation value="Account Information" />
       </div>
     )
   }
